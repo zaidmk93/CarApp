701bb3f5afc5790457259ef8c4b1b9915147355a
Mohamad Zaid <zaidmk93@gmail.com>
2018-12-17 20:22:26 +0200

    static content added !

--- src/main/java/com/carsApp/MohamadCars/MohamadCarsApplication.java
+++ src/main/java/com/carsApp/MohamadCars/MohamadCarsApplication.java
@@ -1,13 +1,19 @@
 package com.carsApp.MohamadCars;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MohamadCarsApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(MohamadCarsApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(MohamadCarsApplication.class, args);
+		logger.info("Hello Spring Boot");
+
 	}
 
 }
