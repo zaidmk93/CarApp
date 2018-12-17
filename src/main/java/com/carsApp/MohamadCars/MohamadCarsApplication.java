60dc7cf4797681f06be9e91c16b05c8d186c5a3a
Mohamad Zaid <zaidmk93@gmail.com>
2018-12-17 19:03:26 +0200

    First Initial commit (Mohamad)

---
+++ src/main/java/com/carsApp/MohamadCars/MohamadCarsApplication.java
@@ -0,0 +1,14 @@
+package com.carsApp.MohamadCars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohamadCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohamadCarsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/carsApp/MohamadCars/MohamadCarsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.carsApp.MohamadCars;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MohamadCarsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
